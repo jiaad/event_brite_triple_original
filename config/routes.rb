2f4ceb960aa97f1261cd0ac01281cbc792e66e8d
jiaad <abdulziad08@outlook.com>
2019-02-13 15:03:02 +0100

    pooppo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root to: "home#index"
   devise_for :users#, :controllers=>{registrations: 'registrations'}
-  resources :events 
+  resources :events do 
+    resources :participants
+  end
   resources :profile, only: [:index , :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
